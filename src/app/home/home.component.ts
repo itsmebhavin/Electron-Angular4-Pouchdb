486078d8aa75db39ce3eeacb664c835d7410ba41
Bhavin Patel <bpatel@caps.ua.edu>
2017-07-31 22:27:11 -0500

    added reactive forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  myform: FormGroup;
+
+  langs: string[] = [
+    'English',
+    'French',
+    'German',
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    // this.myform = new FormGroup({
+    //     name: new FormGroup({
+    //         firstName: new FormControl(),
+    //         lastName: new FormControl(),
+    //     }),
+    //     email: new FormControl(),
+    //     password: new FormControl(),
+    //     language: new FormControl()
+    // });
+
+    //WITH VALIDATIONS
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+      }),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[^ @]*@[^ @]*')
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      language: new FormControl()
+    });
   }
 
 }
