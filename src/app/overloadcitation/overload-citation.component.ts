c8da0a69e3c23ac0d3e96ca0abd43b2bf9fa5e98
CJ Stokes <cjstokes@ua.edu>
2017-08-02 12:25:17 -0500

    Updated form structure.

--- src/app/overloadcitation/overload-citation.component.ts
+++ src/app/overloadcitation/overload-citation.component.ts
@@ -31,36 +31,44 @@ export class OverloadCitationComponent implements OnInit {
                 height: new FormControl('', Validators.required),
                 weight: new FormControl('', Validators.required),
                 licenseNum: new FormControl('', Validators.required),
+                origin: new FormControl('', Validators.required),
+                address: new FormControl('', Validators.required),
+                destination: new FormControl('', Validators.required)
             }),
-            owner: new FormControl('', Validators.required),
-            origin: new FormControl('', Validators.required),
-            address: new FormControl('', Validators.required),
-            destination: new FormControl('', Validators.required),
-            vehicleLicenseNum: new FormControl('', Validators.required),
-            vehicleLicenseState: new FormControl('', Validators.required),
-            vehicleLicenseYear: new FormControl('', Validators.required),
-            vehicleMake: new FormControl('', Validators.required),
-            location: new FormControl('', Validators.required),
-            stateStatute: new FormControl('', Validators.required),
-            totalGross: new FormControl('', Validators.required),
-            bridgeFormula: new FormControl(''),
-            otherViolations: new FormControl(''),
-            legal: new FormControl('', Validators.required),
-            totalWeightViolation: new FormControl('', Validators.required),
-            commodity: new FormControl('', Validators.required),
-            officerOne: new FormControl('', Validators.required),
-            officerTwo: new FormControl('', Validators.required),
-            unitOrStation: new FormControl('', Validators.required),
-            court: new FormControl('', Validators.required),
-            courtCity: new FormControl('', Validators.required),
-            courtDateTime: new FormControl('', Validators.required),
-            bondReceivedBy: new FormControl('', Validators.required),
-            amount: new FormControl('', Validators.required),
-            penalty: new FormControl('', Validators.required),
-            fine: new FormControl('', Validators.required),
-            cost: new FormControl('', Validators.required),
-            billPenaltyTo: new FormControl('', Validators.required),
-            billAddress: new FormControl('', Validators.required)
+            vehicle: new FormGroup({
+                owner: new FormControl('', Validators.required),
+                vehicleLicenseNum: new FormControl('', Validators.required),
+                vehicleLicenseState: new FormControl('', Validators.required),
+                vehicleLicenseYear: new FormControl('', Validators.required),
+                vehicleMake: new FormControl('', Validators.required)
+            }),
+            citation: new FormGroup({
+                location: new FormControl('', Validators.required),
+                stateStatute: new FormControl('', Validators.required),
+                totalGross: new FormControl('', Validators.required),
+                bridgeFormula: new FormControl(''),
+                otherViolations: new FormControl(''),
+                legal: new FormControl('', Validators.required),
+                totalWeightViolation: new FormControl('', Validators.required),
+                commodity: new FormControl('', Validators.required)
+            }),
+            court: new FormGroup({
+                officerOne: new FormControl('', Validators.required),
+                officerTwo: new FormControl('', Validators.required),
+                unitOrStation: new FormControl('', Validators.required),
+                court: new FormControl('', Validators.required),
+                courtCity: new FormControl('', Validators.required),
+                courtDateTime: new FormControl('', Validators.required)
+            }),
+            disposition: new FormGroup({
+                bondReceivedBy: new FormControl('', Validators.required),
+                amount: new FormControl('', Validators.required),
+                penalty: new FormControl('', Validators.required),
+                fine: new FormControl('', Validators.required),
+                cost: new FormControl('', Validators.required),
+                billPenaltyTo: new FormControl('', Validators.required),
+                billAddress: new FormControl('', Validators.required)
+            })
         })
     }
 
