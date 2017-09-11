2e1d84910e6fd7da2829e1bfe0183707a5204da1
Shane Parcus <shane.parcus@ua.edu>
2017-09-11 13:13:04 -0500

    Added basic main menu form tab functionality.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
 	selector: 'app-home',
@@ -13,6 +14,7 @@ export class HomeComponent implements OnInit {
 	public doughnutChartLabels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
 	public doughnutChartData = [350, 450, 100];
 	public doughnutChartType = 'doughnut';
+	public tabs = [];
 
 	// events
 	public chartClicked(e: any): void {
@@ -35,5 +37,14 @@ export class HomeComponent implements OnInit {
 	goto(routerPath) {
 		console.log(routerPath);
 		this._router.navigate(['/layout/' + routerPath]);
+	}
+
+	newForm(newForm) {
+		var ticketNum = moment().format('YYYYMMDDHHmmssSSS'); //Generate ticket number representing this thousandth of a second.
+		this.tabs.push({ form: newForm, ticketNumber: ticketNum });
+	}
+
+	closeForm(index) {
+		this.tabs.splice(index, 1);
 	}
 }
